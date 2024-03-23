61726fd7761b86d5049a880362baf61a71f83c11
Carlos Valdemar Soria <carlosvsoria1982@gmail.com>
2024-03-23 02:23:32 -0300

    React with vite proyect

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const app_port = import.meta.env.VITE_PORT
+console.log(app_port)
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,6 +19,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h4>{app_port}</h4>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
